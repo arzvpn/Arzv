c01102db3969cd458e1f4724fd0a9b27aea812a5
arzvpn <121616001+arzvpn@users.noreply.github.com>
2023-02-08 19:30:28 +0700

    Create autoreboot.sh

---
+++ options/autoreboot.sh
@@ -0,0 +1,248 @@
+#!/bin/bash
+
+BIBlack='\033[1;90m'      # Black
+BIRed='\033[1;91m'        # Red
+BIGreen='\033[1;92m'      # Green
+BIYellow='\033[1;93m'     # Yellow
+BIBlue='\033[1;94m'       # Blue
+BIPurple='\033[1;95m'     # Purple
+BICyan='\033[1;96m'       # Cyan
+BIWhite='\033[1;97m'      # White
+UWhite='\033[4;37m'       # White
+On_IPurple='\033[0;105m'  #
+On_IRed='\033[0;101m'
+IBlack='\033[0;90m'       # Black
+IRed='\033[0;91m'         # Red
+IGreen='\033[0;92m'       # Green
+IYellow='\033[0;93m'      # Yellow
+IBlue='\033[0;94m'        # Blue
+IPurple='\033[0;95m'      # Purple
+ICyan='\033[0;96m'        # Cyan
+IWhite='\033[0;97m'       # White
+NC='\e[0m'
+
+# // Export Color & Information
+export RED='\033[0;31m'
+export GREEN='\033[0;32m'
+export YELLOW='\033[0;33m'
+export BLUE='\033[0;34m'
+export PURPLE='\033[0;35m'
+export CYAN='\033[0;36m'
+export LIGHT='\033[0;37m'
+export NC='\033[0m'
+
+# // Export Banner Status Information
+export EROR="[${RED} EROR ${NC}]"
+export INFO="[${YELLOW} INFO ${NC}]"
+export OKEY="[${GREEN} OKEY ${NC}]"
+export PENDING="[${YELLOW} PENDING ${NC}]"
+export SEND="[${YELLOW} SEND ${NC}]"
+export RECEIVE="[${YELLOW} RECEIVE ${NC}]"
+
+# // Export Align
+export BOLD="\e[1m"
+export WARNING="${RED}\e[5m"
+export UNDERLINE="\e[4m"
+
+# // Exporting URL Host
+export Server_URL="raw.githubusercontent.com/kenDevXD/test/main"
+export Server1_URL="raw.githubusercontent.com/kenDevXD/limit/main"
+export Server_Port="443"
+export Server_IP="underfined"
+export Script_Mode="Stable"
+export Auther=".geovpn"
+
+# // Root Checking
+if [ "${EUID}" -ne 0 ]; then
+		echo -e "${EROR} Please Run This Script As Root User !"
+		exit 1
+fi
+
+# // Exporting IP Address
+export IP=$( curl -s https://ipinfo.io/ip/ )
+
+# // Exporting Network Interface
+export NETWORK_IFACE="$(ip route show to default | awk '{print $5}')"
+
+# // Validate Result ( 1 )
+touch /etc/${Auther}/license.key
+export Your_License_Key="$( cat /etc/${Auther}/license.key | awk '{print $1}' )"
+export Validated_Your_License_Key_With_Server="$( curl -s https://${Server_URL}/validated-registered-license-key.txt | grep -w $Your_License_Key | head -n1 | cut -d ' ' -f 1 )"
+if [[ "$Validated_Your_License_Key_With_Server" == "$Your_License_Key" ]]; then
+    validated='true'
+else
+    echo -e "${EROR} License Key Not Valid"
+    exit 1
+fi
+
+# // Checking VPS Status > Got Banned / No
+if [[ $IP == "$( curl -s https://${Server_URL}/blacklist.txt | cut -d ' ' -f 1 | grep -w $IP | head -n1 )" ]]; then
+    echo -e "${EROR} 403 Forbidden ( Your VPS Has Been Banned )"
+    exit  1
+fi
+
+# // Checking VPS Status > Got Banned / No
+if [[ $Your_License_Key == "$( curl -s https://${Server_URL} | cut -d ' ' -f 1 | grep -w $Your_License_Key | head -n1)" ]]; then
+    echo -e "${EROR} 403 Forbidden ( Your License Has Been Limited )"
+    exit  1
+fi
+
+# // Checking VPS Status > Got Banned / No
+if [[ 'Standart' == "$( curl -s https://${Server_URL}/validated-registered-license-key.txt | grep -w $Your_License_Key | head -n1 | cut -d ' ' -f 6 )" ]]; then 
+    License_Mode='Standart'
+elif [[ Pro == "$( curl -s https://${Server_URL}/validated-registered-license-key.txt | grep -w $Your_License_Key | head -n1 | cut -d ' ' -f 6 )" ]]; then 
+    License_Mode='Pro'
+else
+    echo -e "${EROR} Please Using Genuine License !"
+    exit 1
+fi
+
+# // Checking Script Expired
+exp=$( curl -s https://${Server1_URL}/limit.txt | grep -w $IP | cut -d ' ' -f 3 )
+now=`date -d "0 days" +"%Y-%m-%d"`
+expired_date=$(date -d "$exp" +%s)
+now_date=$(date -d "$now" +%s)
+sisa_hari=$(( ($expired_date - $now_date) / 86400 ))
+if [[ $sisa_hari -lt 0 ]]; then
+    echo $sisa_hari > /etc/${Auther}/license-remaining-active-days.db
+    echo -e "${EROR} Your License Key Expired ( $sisa_hari Days )"
+    exit 1
+else
+    echo $sisa_hari > /etc/${Auther}/license-remaining-active-days.db
+fi
+
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+MYIP=$(wget -qO- icanhazip.com);
+echo "Checking VPS"
+clear
+if [ ! -e /usr/bin/reboot ]; then
+echo '#!/bin/bash' > /usr/bin/reboot
+echo 'tanggal=$(date +"%m-%d-%Y")' >> /usr/bin/reboot
+echo 'waktu=$(date +"%T")' >> /usr/bin/reboot
+echo 'echo "Server successfully rebooted on the date of $tanggal hit $waktu." >> /root/log-reboot.txt' >> /usr/bin/reboot
+echo '/sbin/shutdown -r now' >> /usr/bin/reboot
+chmod +x /usr/bin/reboot
+fi
+
+echo -e ""
+echo -e "------------------------------------" | lolcat
+echo -e "             AUTO REBOOT"
+echo -e "------------------------------------" | lolcat
+echo -e ""
+echo -e "    1)  Auto Reboot 30 Minutes"
+echo -e "    2)  Auto Reboot 1 Hours"
+echo -e "    3)  Auto Reboot 12 Hours"
+echo -e "    4)  Auto Reboot 24 Hours"
+echo -e "    5)  Auto Reboot 1 Weeks"
+echo -e "    6)  Auto Reboot 1 Mount"
+echo -e "    7)  Turn Off Auto Reboot"
+echo -e ""
+echo -e "------------------------------------" | lolcat
+echo -e "    x)   MENU"
+echo -e "------------------------------------" | lolcat
+echo -e ""
+read -p "     Please Input Number  [1-7 or x] :  "  autoreboot
+echo -e ""
+case $autoreboot in
+1)
+echo "*/30 * * * * root /usr/bin/reboot" > /etc/cron.d/auto_reboot && chmod +x /etc/cron.d/auto_reboot
+echo "" > /root/log-reboot.txt
+echo -e ""
+echo -e "======================================"
+echo -e ""
+echo -e "      AutoReboot : On"
+echo -e "      AutoReboot Every : 30 Minutes"
+echo -e ""
+echo -e "======================================"
+service cron reload >/dev/null 2>&1
+service cron restart >/dev/null 2>&1
+;;
+2)
+echo "0 * * * * root /usr/bin/reboot" > /etc/cron.d/auto_reboot && chmod +x /etc/cron.d/auto_reboot
+echo "" > /root/log-reboot.txt
+echo -e ""
+echo -e "======================================"
+echo -e ""
+echo -e "      AutoReboot : On"
+echo -e "      AutoReboot Every : 1 Hours"
+echo -e ""
+echo -e "======================================"
+service cron reload >/dev/null 2>&1
+service cron restart >/dev/null 2>&1
+;;
+3)
+echo "0 */12 * * * root /usr/bin/reboot" > /etc/cron.d/auto_reboot && chmod +x /etc/cron.d/auto_reboot
+echo "" > /root/log-reboot.txt
+echo -e ""
+echo -e "======================================"
+echo -e ""
+echo -e "      AutoReboot : On"
+echo -e "      AutoReboot Every : 12 Hours"
+echo -e ""
+echo -e "======================================"
+service cron reload >/dev/null 2>&1
+service cron restart >/dev/null 2>&1
+;;
+4)
+echo "0 0 * * * root /usr/bin/reboot" > /etc/cron.d/auto_reboot && chmod +x /etc/cron.d/auto_reboot
+echo "" > /root/log-reboot.txt
+echo -e ""
+echo -e "======================================"
+echo -e ""
+echo -e "      AutoReboot : On"
+echo -e "      AutoReboot Every : 24 Hours"
+echo -e ""
+echo -e "======================================"
+service cron reload >/dev/null 2>&1
+service cron restart >/dev/null 2>&1
+;;
+5)
+echo "0 0 */7 * * root /usr/bin/reboot" > /etc/cron.d/auto_reboot && chmod +x /etc/cron.d/auto_reboot
+echo "" > /root/log-reboot.txt
+echo -e ""
+echo -e "======================================"
+echo -e ""
+echo -e "      AutoReboot : On"
+echo -e "      AutoReboot Every : 1 Weeks"
+echo -e ""
+echo -e "======================================"
+service cron reload >/dev/null 2>&1
+service cron restart >/dev/null 2>&1
+;;
+6)
+echo "0 0 1 * * root /usr/bin/reboot" > /etc/cron.d/auto_reboot && chmod +x /etc/cron.d/auto_reboot
+echo "" > /root/log-reboot.txt
+echo -e ""
+echo -e "======================================"
+echo -e ""
+echo -e "      AutoReboot : On"
+echo -e "      AutoReboot Every : 1 Mount"
+echo -e ""
+echo -e "======================================"
+service cron reload >/dev/null 2>&1
+service cron restart >/dev/null 2>&1
+;;
+7)
+rm -fr /etc/cron.d/auto_reboot
+echo "" > /root/log-reboot.txt
+echo -e ""
+echo -e "======================================"
+echo -e ""
+echo -e "      AutoReboot Turned Off"
+echo -e ""
+echo -e "======================================"
+service cron reload >/dev/null 2>&1
+service cron restart >/dev/null 2>&1
+;;
+x)
+menu
+;;
+*)
+echo "Please enter an correct number"
+;;
+esac
+read -n 1 -s -r -p "Press any key to back on menu"
+
+menu
