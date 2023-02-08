31dec1b42a1c65ef2845f6cfaf1109e589e5722f
arzvpn <121616001+arzvpn@users.noreply.github.com>
2023-02-08 21:41:40 +0700

    Update ins-xray.sh

--- tools/ins-xray.sh
+++ tools/ins-xray.sh
@@ -195,9 +195,9 @@ apt install -y nginx
 cd
 rm -fr /etc/nginx/sites-enabled/default
 rm -fr /etc/nginx/sites-available/default
-wget -q -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/kenDevXD/1/main/nginx.conf" 
+wget -q -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/arzvpn/Arzv2/main/options/nginx.conf" 
 #mkdir -p /home/vps/public_html
-wget -q -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/kenDevXD/1/main/vps.conf"
+wget -q -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/arzvpn/Arzv2/main/options/vps.conf"
 
 # Install Xray #
 #==========#
