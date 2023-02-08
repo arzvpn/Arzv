36e457a888f2d6a3b39fdc70e670d7fca2f48940
arzvpn <121616001+arzvpn@users.noreply.github.com>
2023-02-08 21:37:21 +0700

    Update arzsource.sh

--- tools/arzsource.sh
+++ tools/arzsource.sh
@@ -146,6 +146,6 @@ rm -f /root/vnstat-2.6.tar.gz >/dev/null 2>&1
 rm -rf /root/vnstat-2.6 >/dev/null 2>&1
 
 yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
-yellow "JEMBOOT successfully installed..."
+yellow "ArzSource successfully installed..."
 sleep 3
 clear
