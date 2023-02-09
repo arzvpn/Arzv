5e85e190094b6d7e34c8fb178bef7b272f1af630
arzvpn <121616001+arzvpn@users.noreply.github.com>
2023-02-09 19:24:57 +0700

    Update add-vless.sh

--- add-vless.sh
+++ add-vless.sh
@@ -1,35 +1,13 @@
 #!/bin/bash
-
-BIBlack='\033[1;90m'      # Black
-BIRed='\033[1;91m'        # Red
-BIGreen='\033[1;92m'      # Green
-BIYellow='\033[1;93m'     # Yellow
-BIBlue='\033[1;94m'       # Blue
-BIPurple='\033[1;95m'     # Purple
-BICyan='\033[1;96m'       # Cyan
-BIWhite='\033[1;97m'      # White
-UWhite='\033[4;37m'       # White
-On_IPurple='\033[0;105m'  #
-On_IRed='\033[0;101m'
-IBlack='\033[0;90m'       # Black
-IRed='\033[0;91m'         # Red
-IGreen='\033[0;92m'       # Green
-IYellow='\033[0;93m'      # Yellow
-IBlue='\033[0;94m'        # Blue
-IPurple='\033[0;95m'      # Purple
-ICyan='\033[0;96m'        # Cyan
-IWhite='\033[0;97m'       # White
-NC='\e[0m'
-
-# // Export Color & Information
-export RED='\033[0;31m'
-export GREEN='\033[0;32m'
-export YELLOW='\033[0;33m'
-export BLUE='\033[0;34m'
-export PURPLE='\033[0;35m'
-export CYAN='\033[0;36m'
-export LIGHT='\033[0;37m'
-export NC='\033[0m'
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+###########- COLOR CODE -##############
+colornow=$(cat /etc/arzvpn/theme/color.conf)
+NC="\e[0m"
+RED="\033[0;31m" 
+COLOR1="$(cat /etc/arzvpn/theme/$colornow | grep -w "TEXT" | cut -d: -f2|sed 's/ //g')"
+COLBG1="$(cat /etc/arzvpn/theme/$colornow | grep -w "BG" | cut -d: -f2|sed 's/ //g')"                    
+###########- END COLOR CODE -##########
 
 # // Export Banner Status Information
 export EROR="[${RED} EROR ${NC}]"
@@ -45,8 +23,8 @@ export WARNING="${RED}\e[5m"
 export UNDERLINE="\e[4m"
 
 # // Exporting URL Host
-export Server_URL="raw.githubusercontent.com/kenDevXD/test/main"
-export Server1_URL="raw.githubusercontent.com/kenDevXD/limit/main"
+export Server_URL="raw.githubusercontent.com/arzvpn/proarz2/main"
+export Server1_URL="raw.githubusercontent.com/arzvpn/lim2/main"
 export Server_Port="443"
 export Server_IP="underfined"
 export Script_Mode="Stable"
@@ -121,7 +99,7 @@ tls="$(cat ~/log-install.txt | grep -w "Vless TLS" | cut -d: -f2|sed 's/ //g')"
 none="$(cat ~/log-install.txt | grep -w "Vless None TLS" | cut -d: -f2|sed 's/ //g')"
 until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
 echo -e "\033[0;34mттттттттттттттттттттттттттттттттттт\033[0m"
-echo -e "\E[44;1;39m      Add Xray/Vless Account      \E[0m"
+echo -e "\E[44;1;39m     Create Xray/Vless Account      \E[0m"
 echo -e "\033[0;34mттттттттттттттттттттттттттттттттттт\033[0m"
 
 		read -rp "User: " -e user
@@ -130,7 +108,7 @@ echo -e "\033[0;34mтттттттттт�
 		if [[ ${CLIENT_EXISTS} == '1' ]]; then
 clear
 		echo -e "\033[0;34mттттттттттттттттттттттттттттттттттт\033[0m"
-		echo -e "\E[44;1;39m      Add Xray/Vless Account      \E[0m"
+		echo -e "\E[44;1;39m    Create Xray/Vless Account      \E[0m"
 		echo -e "\033[0;34mттттттттттттттттттттттттттттттттттт\033[0m"
 			echo ""
 			echo "A client with the specified name was already created, please choose another name."
@@ -152,28 +130,31 @@ vlesslink2="vless://${uuid}@${domain}:$none?path=/vless&encryption=none&type=ws#
 vlesslink3="vless://${uuid}@${domain}:$tls?mode=gun&security=tls&encryption=none&type=grpc&serviceName=vless-grpc&sni=bug.com#${user}"
 systemctl restart xray
 clear
-echo -e "\033[0;34mттттттттттттттттттттттттттттттттттт\033[0m" | tee -a /etc/log-create-user.log
-echo -e "\E[44;1;39m        Xray/Vless Account        \E[0m" | tee -a /etc/log-create-user.log
-echo -e "\033[0;34mттттттттттттттттттттттттттттттттттт\033[0m" | tee -a /etc/log-create-user.log
-echo -e "Remarks : ${user}" | tee -a /etc/log-create-user.log
-echo -e "Domain : ${domain}" | tee -a /etc/log-create-user.log
-echo -e "port TLS : $tls" | tee -a /etc/log-create-user.log
-echo -e "port none TLS : $none" | tee -a /etc/log-create-user.log
-echo -e "id : ${uuid}" | tee -a /etc/log-create-user.log
-echo -e "Network : ws/grpc" | tee -a /etc/log-create-user.log
-echo -e "Encryption : none" | tee -a /etc/log-create-user.log
-echo -e "Path : /vless" | tee -a /etc/log-create-user.log
-echo -e "Path : vless-grpc" | tee -a /etc/log-create-user.log
-echo -e "\033[0;34mттттттттттттттттттттттттттттттттттт\033[0m" | tee -a /etc/log-create-user.log
-echo -e "Link TLS : ${vlesslink1}" | tee -a /etc/log-create-user.log
-echo -e "\033[0;34mттттттттттттттттттттттттттттттттттт\033[0m" | tee -a /etc/log-create-user.log
-echo -e "Link none TLS : ${vlesslink2}" | tee -a /etc/log-create-user.log
-echo -e "\033[0;34mттттттттттттттттттттттттттттттттттт\033[0m" | tee -a /etc/log-create-user.log
-echo -e "Link GRPC : ${vlesslink3}" | tee -a /etc/log-create-user.log
-echo -e "\033[0;34mттттттттттттттттттттттттттттттттттт\033[0m" | tee -a /etc/log-create-user.log
-echo -e "Expired On : $exp" | tee -a /etc/log-create-user.log
-echo -e "\033[0;34mттттттттттттттттттттттттттттттттттт\033[0m" | tee -a /etc/log-create-user.log
-echo "" | tee -a /etc/log-create-user.log
+echo -e "$COLOR1═════════════XRAY/VLESS═════════════${NC}"
+echo -e "$COLOR1════════════════════════════════════${NC}"
+echo -e "Remarks       : ${user}" 
+echo -e "Expired On    : $exp" 
+echo -e "Domain        : ${domain}" 
+echo -e "port TLS      : $tls" 
+echo -e "port none TLS : $none"
+echo -e "port GRPC     : $tls" 
+echo -e "id            : ${uuid}"
+echo -e "Encryption    : none" 
+echo -e "Network       : ws" 
+echo -e "Path          : /vless" 
+echo -e "Path          : vless-grpc"  
+echo -e "$COLOR1════════════════════════════════════${NC}"
+echo -e "Link TLS :"
+echo -e "${vlesslink1}" 
+echo -e "$COLOR1════════════════════════════════════${NC}"   
+echo -e "Link none TLS : "
+echo -e "${vlesslink2}" 
+echo -e "$COLOR1════════════════════════════════════${NC}"
+echo -e "Link GRPC : "
+echo -e "${vlesslink3}" 
+echo -e "$COLOR1════════════════════════════════════${NC}" 
+echo -e "$COLOR1 Enjoy our Arz Auto Script Service${NC}" 
+echo -e ""
 read -n 1 -s -r -p "Press any key to back on menu"
 
 menu
