849cecc9feeb3f25b09ee0b08e5615eee5743fda
arzvpn <121616001+arzvpn@users.noreply.github.com>
2023-02-15 18:53:40 +0700

    Update set-br.sh

--- backup/set-br.sh
+++ backup/set-br.sh
@@ -123,7 +123,6 @@ chown -R www-data:www-data /etc/msmtprc
 sleep 1
 echo -e "[ ${green}INFO${NC} ] Downloading files... "
 wget -q -O /usr/bin/backup "https://raw.githubusercontent.com/arzvpn/Arzv2/main/backup/backup.sh" && chmod +x /usr/bin/backup
-#wget -q -O /usr/bin/autobackup "https://raw.githubusercontent.com/arzvpn/arzxray/main/autobackup/autobackup.sh" && chmod +x /usr/bin/autobackup
 wget -q -O /usr/bin/restore "https://raw.githubusercontent.com/arzvpn/Arzv2/main/backup/restore.sh" && chmod +x /usr/bin/restore
 
 service cron restart > /dev/null 2>&1
