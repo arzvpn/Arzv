915028dac062b8e1d69ad2558b9ff6b9d5258e02
arzvpn <121616001+arzvpn@users.noreply.github.com>
2023-02-24 17:33:47 +0700

    Update add-ws.sh

--- add-ws.sh
+++ add-ws.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
-dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
-biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
-###########- COLOR CODE -##############
-colornow=$(cat /etc/arzvpn/theme/color.conf)
-NC="\e[0m"
-RED="\033[0;31m" 
-COLOR1="$(cat /etc/arzvpn/theme/$colornow | grep -w "TEXT" | cut -d: -f2|sed 's/ //g')"
-COLBG1="$(cat /etc/arzvpn/theme/$colornow | grep -w "BG" | cut -d: -f2|sed 's/ //g')"                    
-###########- END COLOR CODE -##########
+#Script By Arz
+
+BIBlack='\033[1;90m'      # Black
+BIRed='\033[1;91m'        # Red
+BIGreen='\033[1;92m'      # Green
+BIYellow='\033[1;93m'     # Yellow
+BIBlue='\033[1;94m'       # Blue
+BIPurple='\033[1;95m'     # Purple
+BICyan='\033[1;96m'       # Cyan
+BIWhite='\033[1;97m'      # White
+UWhite='\033[4;37m'       # White
+On_IPurple='\033[0;105m'  #
+On_IRed='\033[0;101m'
+IBlack='\033[0;90m'       # Black
+IRed='\033[0;91m'         # Red
+IGreen='\033[0;92m'       # Green
+IYellow='\033[0;93m'      # Yellow
+IBlue='\033[0;94m'        # Blue
+IPurple='\033[0;95m'      # Purple
+ICyan='\033[0;96m'        # Cyan
+IWhite='\033[0;97m'       # White
+NC='\e[0m'
+green() { echo -e "\\033[32;1m${*}\\033[0m"; }
+red() { echo -e "\\033[31;1m${*}\\033[0m"; }
 
 # // Export Banner Status Information
 export EROR="[${RED} EROR ${NC}]"
@@ -99,22 +114,22 @@ fi
 tls="$(cat ~/log-install.txt | grep -w "Vmess TLS" | cut -d: -f2|sed 's/ //g')"
 none="$(cat ~/log-install.txt | grep -w "Vmess None TLS" | cut -d: -f2|sed 's/ //g')"
 until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
-echo -e "$COLOR1━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e "${BIBlue}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
 echo -e "\\E[0;41;36m    Create Xray/Vmess Account      \E[0m"
-echo -e "$COLOR1━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e "${BIBlue}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
 
 		read -rp "User: " -e user
 		CLIENT_EXISTS=$(grep -w $user /etc/xray/config.json | wc -l)
 
 		if [[ ${CLIENT_EXISTS} == '1' ]]; then
 clear
-            echo -e "$COLOR1━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+            echo -e "${BIBlue}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
             echo -e "\\E[0;41;36m    Create Xray/Vmess Account      \E[0m"
-            echo -e "$COLOR1━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+            echo -e "${BIBlue}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
 			echo ""
 			echo "A client with the specified name was already created, please choose another name."
 			echo ""
-			echo -e "$COLOR1━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+			echo -e "${BIBlue}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
 			read -n 1 -s -r -p "Press any key to back on menu"
 menu
 		fi
@@ -186,14 +201,14 @@ vmesslink3="vmess://$(echo $grpc | base64 -w 0)"
 systemctl restart xray > /dev/null 2>&1
 service cron restart > /dev/null 2>&1
 clear
-echo -e "$COLOR1═════════════XRAY/VMESS═════════════${NC}"
-echo -e "$COLOR1════════════════════════════════════${NC}"
+echo -e "${BIBlue}═════════════XRAY/VMESS═════════════${NC}"
+echo -e "${BIBlue}════════════════════════════════════${NC}"
 echo -e "Remarks       : ${user}"
 echo -e "Expired On    : $exp" 
 echo -e "Domain        : ${domain}" 
-echo -e "Port TLS      : ${tls}" 
-echo -e "Port none TLS : ${none}" 
-echo -e "Port  GRPC    : ${tls}" 
+echo -e "Port TLS      : 443" 
+echo -e "Port none TLS : 80" 
+echo -e "Port  GRPC    : 443" 
 echo -e "id            : ${uuid}" 
 echo -e "alterId       : 0" 
 echo -e "Security      : auto" 
@@ -203,18 +218,17 @@ echo -e "Path          : /worryfree"
 echo -e "Path          : http://bug/worryfree" 
 echo -e "Path          : /kuota-habis" 
 echo -e "ServiceName   : vmess-grpc" 
-echo -e "$COLOR1════════════════════════════════════${NC}" 
-echo -e "$COLOR1════════════════════════════════════${NC}"
+echo -e "${BIBlue}════════════════════════════════════${NC}" 
 echo -e "Link TLS : "
 echo -e "${vmesslink1}" 
-echo -e "$COLOR1════════════════════════════════════${NC} "
+echo -e "${BIBlue}════════════════════════════════════${NC} "
 echo -e "Link none TLS : "
 echo -e "${vmesslink2}" 
-echo -e "$COLOR1════════════════════════════════════${NC} "
+echo -e "${BIBlue}════════════════════════════════════${NC} "
 echo -e "Link GRPC : "
 echo -e "${vmesslink3}"
-echo -e "$COLOR1════════════════════════════════════${NC}" 
-echo -e "$COLOR1 Enjoy our Arz Auto Script Service${NC}" 
+echo -e "${BIBlue}════════════════════════════════════${NC}" 
+echo -e "${BICyan} Enjoy our Arz Auto Script Service${NC}" 
 echo "" | tee -a /etc/log-create-user.log
 rm /etc/xray/$user-tls.json > /dev/null 2>&1
 rm /etc/xray/$user-none.json > /dev/null 2>&1
