cb15ba8fb22a213ccd7ef99f3cb10b17ac05aa76
arzvpn <121616001+arzvpn@users.noreply.github.com>
2023-02-24 18:13:01 +0700

    Update add-ws.sh

--- add-ws.sh
+++ add-ws.sh
@@ -114,18 +114,18 @@ fi
 tls="$(cat ~/log-install.txt | grep -w "Vmess TLS" | cut -d: -f2|sed 's/ //g')"
 none="$(cat ~/log-install.txt | grep -w "Vmess None TLS" | cut -d: -f2|sed 's/ //g')"
 until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
-echo -e "${BIBlue}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
-echo -e "\\E[0;41;36m    Create Xray/Vmess Account      \E[0m"
-echo -e "${BIBlue}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e "\033[0;34m┌─────────────────────────────────────────────────┐\033[0m"
+echo -e "\\E[0;41;36m            Create Xray/Vmess Account             \E[0m"
+echo -e "\033[0;34m└─────────────────────────────────────────────────┘\033[0m"
 
 		read -rp "User: " -e user
 		CLIENT_EXISTS=$(grep -w $user /etc/xray/config.json | wc -l)
 
 		if [[ ${CLIENT_EXISTS} == '1' ]]; then
 clear
-            echo -e "${BIBlue}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
-            echo -e "\\E[0;41;36m    Create Xray/Vmess Account      \E[0m"
-            echo -e "${BIBlue}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+            echo -e "\033[0;34m┌─────────────────────────────────────────────────┐\033[0m"
+            echo -e "\\E[0;41;36m            Create Xray/Vmess Account             \E[0m"
+            echo -e "\033[0;34m└─────────────────────────────────────────────────┘\033[0m"
 			echo ""
 			echo "A client with the specified name was already created, please choose another name."
 			echo ""
@@ -201,8 +201,8 @@ vmesslink3="vmess://$(echo $grpc | base64 -w 0)"
 systemctl restart xray > /dev/null 2>&1
 service cron restart > /dev/null 2>&1
 clear
-echo -e "${BIBlue}═════════════XRAY/VMESS═════════════${NC}"
-echo -e "${BIBlue}════════════════════════════════════${NC}"
+echo -e "${BIBlue}═══════════XRAY/VMESS═══════════${NC}"
+echo -e "${BIBlue}════════════════════════════════${NC}"
 echo -e "Remarks       : ${user}"
 echo -e "Expired On    : $exp" 
 echo -e "Domain        : ${domain}" 
@@ -218,16 +218,16 @@ echo -e "Path          : /worryfree"
 echo -e "Path          : http://bug/worryfree" 
 echo -e "Path          : /kuota-habis" 
 echo -e "ServiceName   : vmess-grpc" 
-echo -e "${BIBlue}════════════════════════════════════${NC}" 
+echo -e "${BIBlue}════════════════════════════════${NC}" 
 echo -e "Link TLS : "
 echo -e "${vmesslink1}" 
-echo -e "${BIBlue}════════════════════════════════════${NC} "
+echo -e "${BIBlue}════════════════════════════════${NC} "
 echo -e "Link none TLS : "
 echo -e "${vmesslink2}" 
-echo -e "${BIBlue}════════════════════════════════════${NC} "
+echo -e "${BIBlue}════════════════════════════════${NC} "
 echo -e "Link GRPC : "
 echo -e "${vmesslink3}"
-echo -e "${BIBlue}════════════════════════════════════${NC}" 
+echo -e "${BIBlue}════════════════════════════════${NC}" 
 echo -e "${BICyan} Enjoy our Arz Auto Script Service${NC}" 
 echo "" | tee -a /etc/log-create-user.log
 rm /etc/xray/$user-tls.json > /dev/null 2>&1
