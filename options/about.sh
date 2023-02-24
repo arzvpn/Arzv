dbce1aec19bac7bcaee0a9b22a89a45f310c5416
arzvpn <121616001+arzvpn@users.noreply.github.com>
2023-02-24 18:20:24 +0700

    Update about.sh

--- options/about.sh
+++ options/about.sh
@@ -1,4 +1,4 @@
- echo -e "          *               **************            *********************
+ echo -e "\033[0;34m          *               **************            *********************
          *  *             *              *                           *
         *    *            *               *                        *
        *      *           *             *                        *
@@ -8,17 +8,17 @@
    *              *       *               *              *
   *                *      *                *           *
  *                  *     *                 *        *
-*                    *    *                  *    *********************
+*                    *    *                  *    ********************* \033[0m"
 
 
->>>>>>>>>>Welcome to AutoScript Premium V1<<<<<<<<<<
->>>>>>>>>>>>>>>>>>AUTOSCRIPT BY Arz<<<<<<<<<<<<<<<<<
+echo -e "\033[0;94m>>>>>>>>>>Welcome to AutoScript Premium V2<<<<<<<<<<
+>>>>>>>>>>>>>>>>>>AUTOSCRIPT BY Arz<<<<<<<<<<<<<<<<<\e[0m"
 
->Support OS 
-For Ubuntu 18 & Ubuntu 20 
-For Debian 9 & Debian 10
+echo -e ">Support OS 
+For Ubuntu 18.04 & Ubuntu 20.04 & Ubuntu 22.04 
+For Debian 9 & Debian 10 & Debian 11 "
 
-Contact Owner AutoScript
+echo -e "Contact Owner AutoScript
 > WA: 083117634078
 > TELEGRAM: t.me/Store_Arz"
 
