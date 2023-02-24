31212cf950fb672085cca2ac3ac4e96bf0a5ba67
arzvpn <121616001+arzvpn@users.noreply.github.com>
2023-02-24 18:07:13 +0700

    Update add-ssws.sh

--- add-ssws.sh
+++ add-ssws.sh
@@ -1,13 +1,51 @@
 #!/bin/bash
-dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
-biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
-###########- COLOR CODE -##############
-colornow=$(cat /etc/arzvpn/theme/color.conf)
-NC="\e[0m"
-RED="\033[0;31m" 
-COLOR1="$(cat /etc/arzvpn/theme/$colornow | grep -w "TEXT" | cut -d: -f2|sed 's/ //g')"
-COLBG1="$(cat /etc/arzvpn/theme/$colornow | grep -w "BG" | cut -d: -f2|sed 's/ //g')"                    
-###########- END COLOR CODE -##########
+#Script By Arz
+
+BIBlack='\033[1;90m'      # Black
+BIRed='\033[1;91m'        # Red
+BIGreen='\033[1;92m'      # Green
+BIYellow='\033[1;93m'     # Yellow
+BIBlue='\033[1;94m'       # Blue
+BIPurple='\033[1;95m'     # Purple
+BICyan='\033[1;96m'       # Cyan
+BIWhite='\033[1;97m'      # White
+UWhite='\033[4;37m'       # White
+On_IPurple='\033[0;105m'  #
+On_IRed='\033[0;101m'
+IBlack='\033[0;90m'       # Black
+IRed='\033[0;91m'         # Red
+IGreen='\033[0;92m'       # Green
+IYellow='\033[0;93m'      # Yellow
+IBlue='\033[0;94m'        # Blue
+IPurple='\033[0;95m'      # Purple
+ICyan='\033[0;96m'        # Cyan
+IWhite='\033[0;97m'       # White
+NC='\e[0m'
+green() { echo -e "\\033[32;1m${*}\\033[0m"; }
+red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+
+# // Export Color & Information
+export RED='\033[0;31m'
+export GREEN='\033[0;32m'
+export YELLOW='\033[0;33m'
+export BLUE='\033[0;34m'
+export PURPLE='\033[0;35m'
+export CYAN='\033[0;36m'
+export LIGHT='\033[0;37m'
+export NC='\033[0m'
+
+# // Export Banner Status Information
+export EROR="[${RED} EROR ${NC}]"
+export INFO="[${YELLOW} INFO ${NC}]"
+export OKEY="[${GREEN} OKEY ${NC}]"
+export PENDING="[${YELLOW} PENDING ${NC}]"
+export SEND="[${YELLOW} SEND ${NC}]"
+export RECEIVE="[${YELLOW} RECEIVE ${NC}]"
+
+# // Export Align
+export BOLD="\e[1m"
+export WARNING="${RED}\e[5m"
+export UNDERLINE="\e[4m"
 
 # // Exporting URL Host
 export Server_URL="raw.githubusercontent.com/arzvpn/proarzv2/main"
@@ -87,24 +125,23 @@ fi
 
 tls="$(cat ~/log-install.txt | grep -w "Sodosok WS/GRPC" | cut -d: -f2|sed 's/ //g')"
 until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
-echo -e "\033[0;34mג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”\033[0m"
-echo -e "\\E[0;41;36m     Create Sodosok Ws/Grpc Account    \E[0m"
-echo -e "\033[0;34mג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”\033[0m"
+echo -e "\033[0;34m┌─────────────────────────────────────────────────┐\033[0m"
+echo -e "\\E[0;41;36m           Create Sodosok Ws/Grpc Account         \E[0m"
+echo -e "\033[0;34m└─────────────────────────────────────────────────┘\033[0m"
 
 		read -rp "User: " -e user
 		CLIENT_EXISTS=$(grep -w $user /etc/xray/config.json | wc -l)
 
 		if [[ ${CLIENT_EXISTS} == '1' ]]; then
 clear
-            echo -e "\033[0;34mג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”\033[0m"
-            echo -e "\\E[0;41;36m     Create Sodosok Ws/Grpc Account      \E[0m"
-            echo -e "\033[0;34mג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”\033[0m"
+            echo -e "\033[0;34m┌─────────────────────────────────────────────────┐\033[0m"
+            echo -e "\\E[0;41;36m          Create Sodosok Ws/Grpc Account          \E[0m"
+            echo -e "\033[0;34m└─────────────────────────────────────────────────┘\033[0m"
 			echo ""
 			echo "A client with the specified name was already created, please choose another name."
 			echo ""
-			echo -e "\033[0;34mג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”ג”\033[0m"
 			read -n 1 -s -r -p "Press any key to back on menu"
-v2ray-menu
+menu-ss
 		fi
 	done
 
@@ -350,8 +387,8 @@ END
 systemctl restart xray > /dev/null 2>&1
 service cron restart > /dev/null 2>&1
 clear
-echo -e "$COLOR1═════════════XRAY/SSWS══════════════${NC}"
-echo -e "$COLOR1════════════════════════════════════${NC}"
+echo -e "${BIBlue}════════════XRAY/SSWS═════════════${NC}"
+echo -e "${BIBlue}══════════════════════════════════${NC}"
 echo -e "Remarks      : ${user}" 
 echo -e "Expired On   : $exp"  
 echo -e "Domain       : ${domain}"  
@@ -362,14 +399,14 @@ echo -e "Cipers       : aes-128-gcm"
 echo -e "Network      : ws/grpc"  
 echo -e "Path         : /ss-ws"  
 echo -e "ServiceName  : ss-grpc"  
-echo -e "$COLOR1════════════════════════════════════${NC}" 
+echo -e "${BIBlue}══════════════════════════════════${NC}" 
 echo -e "Link TLS : "
 echo -e "${shadowsockslink}"  
-echo -e "$COLOR1════════════════════════════════════${NC} "
+echo -e "${BIBlue}══════════════════════════════════${NC} "
 echo -e "Link GRPC : "
 echo -e "${shadowsockslink1}"  
-echo -e "$COLOR1════════════════════════════════════${NC} "
-echo -e "$COLOR1 Enjoy our Arz Auto Script Service${NC}" 
+echo -e "${BIBlue}══════════════════════════════════${NC} "
+echo -e "${BICyan} Enjoy our Arz Auto Script Service${NC}" 
 echo ""  
 read -n 1 -s -r -p "Press any key to back on menu"
 
