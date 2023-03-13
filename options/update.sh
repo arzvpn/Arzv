3a74afc6fc75dc79e16d510d6dbfd587c3ff8365
arzvpn <121616001+arzvpn@users.noreply.github.com>
2023-03-13 20:16:47 +0700

    Update update.sh

--- options/update.sh
+++ options/update.sh
@@ -120,7 +120,7 @@ wget -q -O /usr/bin/xp "https://raw.githubusercontent.com/arzvpn/Arzv/main/xp.sh
 wget -q -O /usr/bin/update "https://raw.githubusercontent.com/arzvpn/Arzv/main/options/update.sh"
 wget -q -O /usr/bin/menu-theme "https://raw.githubusercontent.com/arzvpn/Arzv/main/menu/menu-theme.sh"
 wget -q -O /usr/bin/info "https://raw.githubusercontent.com/arzvpn/Arzv/main/options/info.sh"
-wget -q -O /usr/bin/info "https://raw.githubusercontent.com/arzvpn/Arzv/main/options/infoserv.sh"
+wget -q -O /usr/bin/infoserv "https://raw.githubusercontent.com/arzvpn/Arzv/main/options/infoserv.sh"
 chmod +x /usr/bin/add-ws
 chmod +x /usr/bin/add-ssws
 chmod +x /usr/bin/add-vless
