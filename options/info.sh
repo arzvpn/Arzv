04f0fa7a63eb5d92625170927bfe6194663e9c1a
arzvpn <121616001+arzvpn@users.noreply.github.com>
2023-03-14 22:05:01 +0700

    Update info.sh

--- options/info.sh
+++ options/info.sh
@@ -23,6 +23,11 @@ IWhite='\033[0;97m'       # White
 NC='\e[0m'
 green() { echo -e "\\033[32;1m${*}\\033[0m"; }
 red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+cpu_usage1="$(ps aux | awk 'BEGIN {sum=0} {sum+=$3}; END {print sum}')"
+cpu_usage="$((${cpu_usage1/\.*} / ${corediilik:-1}))"
+cpu_usage+=" %"
+tram=$( free -h | awk 'NR==2 {print $2}' )
+uram=$( free -h | awk 'NR==2 {print $3}' )
 
 # // Export Color & Information
 export RED='\033[0;31m'
@@ -47,10 +52,106 @@ export BOLD="\e[1m"
 export WARNING="${RED}\e[5m"
 export UNDERLINE="\e[4m"
 
+BURIQ () {
+    curl -sS https://raw.githubusercontent.com/arzvpn/permission/main/ip > /root/tmp
+    data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
+    for user in "${data[@]}"
+    do
+    exp=( `grep -E "^### $user" "/root/tmp" | awk '{print $3}'` )
+    d1=(`date -d "$exp" +%s`)
+    d2=(`date -d "$biji" +%s`)
+    exp2=$(( (d1 - d2) / 86400 ))
+    if [[ "$exp2" -le "0" ]]; then
+    echo $user > /etc/.$user.ini
+    else
+    rm -f /etc/.$user.ini > /dev/null 2>&1
+    fi
+    done
+    rm -f /root/tmp
+}
+
+MYIP=$(curl -sS ipv4.icanhazip.com)
+Name=$(curl -sS https://raw.githubusercontent.com/arzvpn/permission/main/ip | grep $MYIP | awk '{print $2}')
+Isadmin=$(curl -sS https://raw.githubusercontent.com/arzvpn/permission/main/ip | grep $MYIP | awk '{print $5}')
+echo $Name > /usr/local/etc/.$Name.ini
+CekOne=$(cat /usr/local/etc/.$Name.ini)
+
+Bloman () {
+if [ -f "/etc/.$Name.ini" ]; then
+CekTwo=$(cat /etc/.$Name.ini)
+    if [ "$CekOne" = "$CekTwo" ]; then
+        res="Expired"
+    fi
+else
+res="Permission Accepted..."
+fi
+}
+
+PERMISSION () {
+    MYIP=$(curl -sS ipv4.icanhazip.com)
+    IZIN=$(curl -sS https://raw.githubusercontent.com/arzvpn/permission/main/ip | awk '{print $4}' | grep $MYIP)
+    if [ "$MYIP" = "$IZIN" ]; then
+    Bloman
+    else
+    res="Permission Denied!"
+    fi
+    BURIQ
+}
+if [ "$res" = "Expired" ]; then
+Exp="\e[36mExpired\033[0m"
+rm -f /home/needupdate > /dev/null 2>&1
+else
+Exp=$(curl -sS https://raw.githubusercontent.com/arzvpn/permission/main/ip | grep $MYIP | awk '{print $3}')
+fi
+
+DATE=$(date +'%d %B %Y')
+datediff() {
+    d1=$(date -d "$1" +%s)
+    d2=$(date -d "$2" +%s)
+    echo -e "\033[1;96m  Expires in   : $(( (d1 - d2) / 86400 )) Days"
+}
+mai="datediff "$Exp" "$DATE""
+
 clear
-echo -e "$BICyan┌──────────────────────────────────────────┐${NC}"
-echo -e "$BICyan│           SERVER PORT INFORMATION        │$NC"       
-echo -e "$BICyan└──────────────────────────────────────────┘${NC}"
+echo -e "┌──────────────────────────────────────────┐" | lolcat
+echo -e "│            SERVER INFORMATION            │" | lolcat
+echo -e "└──────────────────────────────────────────┘" | lolcat
+echo -e "\033[1;91m  IP VPS        : $(curl -s ipinfo.io/ip ) \e[0m"
+uphours=`uptime -p | awk '{print $2,$3}' | cut -d , -f1`
+upminutes=`uptime -p | awk '{print $4,$5}' | cut -d , -f1`
+uptimecek=`uptime -p | awk '{print $6,$7}' | cut -d , -f1`
+cekup=`uptime -p | grep -ow "day"`
+if [ "$cekup" = "day" ]; then
+echo -e "\033[1;91m  SYSTEM UPTIME : $uphours $upminutes $uptimecek \e[0m"
+else
+echo -e "\033[1;91m  SYSTEM UPTIME : $uphours $upminutes \e[0m"
+fi
+echo -e "\033[1;91m  OS VPS        : "`hostnamectl | grep "Operating System" | cut -d ' ' -f5-` $NC
+echo -e "\033[1;91m  MEMORY USAGE  : $uram / $tram \e[0m"
+echo -e "\033[1;91m  ISP VPS       : $(curl -s ipinfo.io/org | cut -d " " -f 2-10 ) \e[0m"
+echo -e "\033[1;91m  REGION        : $(curl -s ipinfo.io/timezone ) \e[0m"
+echo -e "\033[1;91m  CITY          : $(curl -s ipinfo.io/city ) \e[0m"
+echo -e "\033[1;91m  DOMAIN        : $(cat /etc/xray/domain)\e[0m"
+echo -e "\033[1;91m  DATE&TIME     : $( date -d "0 days" +"%d-%m-%Y | %X" )\e[0m"
+echo -e "┌──────────────────────────────────────────┐" | lolcat
+echo -e "│           AUTOSCRIPT INFORMATION         │" | lolcat       
+echo -e "└──────────────────────────────────────────┘" | lolcat
+echo -e "  Owner        : Arz Vpn Store " | lolcat
+echo -e "\033[1;96m  Contact WA   : 083117634078 \e[0m"
+echo -e "\033[1;96m  Telegram     : t.me/Store_Arz \e[0m"
+echo -e "\033[1;96m  Official     : t.me/arzvpn \e[0m"
+echo -e "\033[1;96m  Type Script  : Multiport V2 \e[0m"
+echo -e "\033[1;96m  Version SC   : $(cat /opt/.ver) \e[0m"
+echo -e "\033[1;96m  Client Name  : $Name \e[0m"
+if [ $Exp \< 1000 ];
+then
+echo -e "\033[1;96m  License     : $sisa_hari$NC Days Tersisa \e[0m"
+else
+    datediff "$Exp" "$DATE"
+fi;
+echo -e "┌──────────────────────────────────────────┐" | lolcat
+echo -e "│           SERVER PORT INFORMATION        │" | lolcat      
+echo -e "└──────────────────────────────────────────┘" | lolcat
 echo -e "\033[1;93m  >Port SSH Websocket       :80\e[0m"
 echo -e "\033[1;93m  >Port SSH Websocket SSL   :443\e[0m"
 echo -e "\033[1;93m  >Port SSH SSL             :447,777\e[0m"
