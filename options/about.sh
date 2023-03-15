bc74e279df165334dbdb53615f3a5d57db2cb848
arzvpn <121616001+arzvpn@users.noreply.github.com>
2023-03-15 11:27:38 +0700

    Create about.sh

---
+++ options/about.sh
@@ -0,0 +1,29 @@
+ echo -e "    
+         *              **************           *******************
+        * *             *             *                         *
+       *   *            *              *                      *
+      *     *           *            *                      *
+     *       *          *************                     *
+    ***********         *            *                  *
+   *           *        *             *               *
+  *             *       *              *            *
+ *               *      *               *         *
+*                 *     *                *      ******************** " | lolcat
+
+
+echo -e ">>>>>>>>>>Welcome to AutoScript Premium V2<<<<<<<<<<
+>>>>>>>>>>>>>>>>>>AUTOSCRIPT BY Arz<<<<<<<<<<<<<<<<<" | lolcat
+
+echo -e ">Support OS 
+For Ubuntu 18.04 & Ubuntu 20.04 & Ubuntu 22.04 
+For Debian 9 & Debian 10 & Debian 11 " | lolcat
+
+echo -e "Contact Owner AutoScript
+> WA: 083117634078
+> TELEGRAM: t.me/Store_Arz" | lolcat
+
+echo -e "Thanks for your Order Arz AutoScript" | lolcat
+echo ""
+read -n 1 -s -r -p "  Press any key to Back to Menu"
+menu
+fi
