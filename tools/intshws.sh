d07952a15c32269feed427ac24182b6401632cd0
arzvpn <121616001+arzvpn@users.noreply.github.com>
2023-09-05 21:45:27 +0700

    Create intshws.sh

---
+++ tools/intshws.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#installer Websocker tunneling 
+
+cd
+
+#Install Script Websocket-SSH Python
+wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/arzvpn/Arzv/main/tools/ws-stunnel
+
+#izin permision
+chmod +x /usr/local/bin/ws-stunnel
+
+#System SSL/TLS Websocket-SSH Python
+wget -O /etc/systemd/system/ws-stunnel.service https://raw.githubusercontent.com/arzvpn/Arzv/main/tools/ws-stunnel.service && chmod +x /etc/systemd/system/ws-stunnel.service
+
+#restart service
+systemctl daemon-reload
+#Enable & Start ws-stunnel service
+systemctl enable ws-stunnel.service
+systemctl start ws-stunnel.service
+systemctl restart ws-stunnel.service
